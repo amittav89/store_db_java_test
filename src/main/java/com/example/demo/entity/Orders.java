531b84a3a23abcda27acc558f563918ff0a156be
amittav89 <120167154+amittav89@users.noreply.github.com>
2023-05-26 19:09:09 +0300

    add table orders

---
+++ src/main/java/com/example/demo/entity/Orders.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_no", nullable = false)
+    private Long order_no;
+
+    /*relationships -- many to one*/
+    @Column(name = "customer_id", nullable = false)
+    private Long customer_id;
+
+    @Column(name = "order_date", nullable = false)
+    private Date order_date;
+
+}
