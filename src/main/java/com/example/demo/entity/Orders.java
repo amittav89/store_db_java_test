5a58a69a42588427248ed98769dced0e98108074
amittav89 <120167154+amittav89@users.noreply.github.com>
2023-06-05 13:34:45 +0300

    delete OrderDetails, add relationship table order_product

--- src/main/java/com/example/demo/entity/Orders.java
+++ src/main/java/com/example/demo/entity/Orders.java
@@ -7,7 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import jakarta.persistence.JoinTable;
 
 import java.util.Date;
 
@@ -21,15 +26,19 @@ public class Orders {
     private Long order_no;
 
     /*relationships -- many to one*/
-
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customers customer;
 
-   /* @Column(name = "customer_id", nullable = false)
-    private Long customer_id;*/
-
     @Column(name = "order_date", nullable = false)
     private Date order_date;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Products> products = new HashSet<>();
 
 }
--- src/main/java/com/example/demo/entity/Products.java
+++ src/main/java/com/example/demo/entity/Products.java
@@ -1,12 +1,20 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import org.hibernate.annotations.Check;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
-
 @Check(constraints = "product_price>0")
 public class Products {
     @Id
@@ -27,6 +35,9 @@ public class Products {
     @Column(name = "supplier_id", nullable = false)
     private Long supplierId;
 
+    @ManyToMany(mappedBy = "products")
+    private Set<Orders> orders = new HashSet<>();
+
     public Products(String productName, double productPrice, int productStock) {
         this.productName = productName;
         this.productPrice = productPrice;
@@ -36,4 +47,6 @@ public class Products {
     public Products() {
 
     }
+
+
 }
